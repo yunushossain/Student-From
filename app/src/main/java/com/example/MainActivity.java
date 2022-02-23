c4b312196272125328745105008b13021f2fa188
yunushossain <yunushossain07@gmail.com>
2022-02-23 13:31:51 +0600

    change

--- app/src/main/java/com/example/MainActivity.java
+++ app/src/main/java/com/example/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
 
-
     EditText name, email, phone, address;
+    RadioGroup gen;
     Button submit;
+    RadioButton selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,18 +26,21 @@ public class MainActivity extends AppCompatActivity {
         phone = findViewById(R.id.tfPhone);
         address = findViewById(R.id.tfAddress);
         submit = findViewById(R.id.btnSubmit);
+        gen = findViewById(R.id.rgGender);
+
 
         submit.setOnClickListener(
                 view -> {
+                    selected = findViewById(gen.getCheckedRadioButtonId());
                     System.out.println(name.getText());
                     System.out.println(email.getText());
                     System.out.println(phone.getText());
                     System.out.println(address.getText());
+                    System.out.println(selected.getText().toString());
                 }
         );
 
     }
-
 
 
 }
